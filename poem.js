82990882a7fde1351a49a9b70eed0eed77446540
A-TrainStation <149599224+A-TrainStation@users.noreply.github.com>
2024-06-26 17:54:32 -0500

    Add files via upload
    My first one is a beloved Poem

---
+++ poem.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const poem = `DREAMING PAST THE DAYS I LOVE, playing baseball with my glove.
+Bouncing Ideas off a wall, 2 day's later I helped the mall.
+Have any concerns please call, I will see you Next Years Fall.
+Moving on to something good, reading books like Red Riding Hood.
+Looking back at who I am, thanking my town as my fans.
+Feeling stronger like a man. Having such a great Career, couldn't done it without my fears.
+At least everyone's gone through tears. Taking Words of advice from me, don't you worry your safe with me.
+Come to me and 1 / 1 1 show you, see. Picking out one Memory, being with my family.
+Hope you all decide to agree. Staying with my easy flow, one wrong turn and your down below.
+Why is every answer No?. Please C'mon my heart is low.
+Taking someone out tonight, love the kindness and polite. Has someone told you're bright.
+Sending wish cards overseas, having my family come see. The entire family tree.
+Stopping at a perfect time, really shares joy in my Rhymes, hate when people do bad Crimes.
+Selling out my works of art, turning scratch in a new fresh start.
+Taking these words from my heart, hopes this song never falls apart.
+THIS IS MY MOMENT WITH YOU, ONE LAST THING LETS ALL STAY TRUE.`;
+
+    document.getElementById("poem").textContent = poem;
+});
